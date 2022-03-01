a813e6ee5f92db2a1ab49fd2667c7feb3a971900
huiyuan.dong <huiyuan.dong@outlook.com>
2022-03-01 23:33:21 +0800

    v1.0

--- src/main.cpp
+++ src/main.cpp
@@ -5,15 +5,19 @@
 #include <SoftwareSerial.h>
 SoftwareSerial soft_ware(A0,A1);  //设置软串口
 
+String aliyun="{\"method\":\"thing.event.property.post\",\"id\":\"123\",\"params\":{\"PM25\":";
+
+String ali = "},\"version\":\"1.0\"}";
+
 char data[4]={0X56,0X71,0X00,0X27}; //设置
 char dataa[4]={0X56,0X11,0X00,0X47}; //设置
 char dataaa[4]={0X56,0X35,0X00,0X63}; //
-
+int zhongjian;
 
 void setup() {
   // put your setup code here, to run once:
   soft_ware.begin(9600);
-  Serial.begin(9600);
+  Serial.begin(115200);
   soft_ware.write(data,4);
   soft_ware.write(dataa,4);
   soft_ware.write(dataaa,4);
@@ -32,8 +36,15 @@ void loop() {
     {  
       //这段代码实现从缓冲区读取数据，并将数据发送到计算机显示和软串口发送；
       soft_ware.read();
-      Serial.print(device_A_String);
-      delay(200);
+      int a = device_A_String.substring(7,12).toInt();
+      if((zhongjian-a)>1000){
+        Serial.print(aliyun+String(a)+ali);
+        delay(400);
+        zhongjian = a;
+      }
+      else{
+        zhongjian = a;
+      }
       device_A_String="";
     }
   }
